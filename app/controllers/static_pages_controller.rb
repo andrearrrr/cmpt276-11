5e013da0e244e5bb7ebaf6cc77ec6cbc1ca2b39f
Andrea Renney <andrea.renney@hotmail.com>
2017-10-29 16:06:56 -0700

    chapter 3

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,18 @@
+#StaticPagesController inherits from ApplicationController
 class StaticPagesController < ApplicationController
+
+#these actions are functions/methods
+
   def home
   end
 
   def help
   end
+
+  def about
+  end
+
+  def contact
+  end
+
 end
