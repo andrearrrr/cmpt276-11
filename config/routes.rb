84ba51ae638029e4c737d4800810275ef7bb466c
zullycomas <zullycomas32@gmail.com>
2017-11-03 00:34:27 -0700

    Fixing Merge conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'users/login'
-
-  get 'users/new'
 
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
@@ -13,5 +10,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
   resources :users
+  # get 'users/login'
+  # get 'users/new'
+
 
 end
