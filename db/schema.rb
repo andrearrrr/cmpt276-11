6ca4e21af2de57411d6d96876b0998f11633235b
shind07 <shind@sfu.ca>
2017-11-06 15:01:53 -0800

    Seeded Players objects

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171103232533) do
+ActiveRecord::Schema.define(version: 20171106224057) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "players", force: :cascade do |t|
+    t.string "name"
+    t.string "identifier"
+    t.string "position"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,23 @@ User.create(name: "Admin User", email: "admin@test.com", password: "cmpt276", ad
 require 'csv'
 
 csv_text = File.read(Rails.root.join('lib', 'seeds', 'player_data.csv'))
-csv = CSV.parse(csv_text, :headers => true, :encoding => 'utf-8')
+$csv = CSV.parse(csv_text, :headers => true, :encoding => 'utf-8')
 
-players = []
-csv.take(7).each do |row|
-	if !players.include? row['Player']
-	 	puts row['Player']
-		players << row['Player']
-	#else puts row['Player']
+def seed_players
+	$csv.each do |row|
+		identifier = row['Identifier']
+		name = row['Player']
+		position = row['Pos']
+		puts identifier
+		if !Player.where(:identifier => identifier).exists?
+			puts name, identifier
+			p = Player.new
+			p.name = name
+			p.identifier = identifier
+			p.position = position
+			puts p.to_json
+			p.save
+			print p.save
+		end
 	end
-	#puts row['Player']
 end
