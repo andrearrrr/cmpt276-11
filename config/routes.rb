ec59c894931f76790dc31cbdcadba51d7d5fa3af
shind07 <shind@sfu.ca>
2017-11-06 15:18:09 -0800

    Create Players controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
 
 
-  get 'sessions/new'
-
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
@@ -10,7 +8,11 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+
+  get '/players', to: 'players#index'
+  get '/players/:id', to: 'players#show', as: "player"
   resources :users
+#  resources :players
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
