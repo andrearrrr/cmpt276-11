a053cfdd63a229885a72aaaf4df3bfd7980bb908
Andrea Renney <andrea.renney@hotmail.com>
2017-11-18 10:49:18 -0800

    mail service changed, need to test in production, merging to master

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,14 +27,21 @@ Rails.application.configure do
   end
 
   #Mailer stuff...
-
-  config.action_mailer.perform_caching = false
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :test
-  # Use your local dev host instead
-  # What is our local dev host?
+  config.action_mailer.delivery_method = :smtp
   host = 'localhost:5000'
-  config.action_mailer.default_url_options = { host: 'example.com' }
+  config.action_mailer.default_url_options =
+  { host: 'localhost:5000' }
+
+  config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: ENV["GMAIL_DOMAIN"],
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: ENV["GMAIL_USERNAME"],
+  password: ENV["GMAIL_PASSWORD"]
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
