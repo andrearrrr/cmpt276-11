f037c734a67d24e47138a426473bf25e05f89faa
zullycomas <zullycomas32@gmail.com>
2017-11-17 19:02:26 -0800

    Creating posts from Home Page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,8 +2,9 @@
 class StaticPagesController < ApplicationController
 
 #these actions are functions/methods
-
   def home
+  	# Defien variable to use in micropost form
+    @micropost = current_user.microposts.build if logged_in?
   end
 
   def help
