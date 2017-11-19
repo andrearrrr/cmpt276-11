f6af09c2680dfc2d56d19e06067542e7b06e7248
Bryce-C <brycechutskoff@gmail.com>
2017-11-18 20:27:14 -0800

    Views for outgoing/incoming friends

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy,
+  #adding some logic for friends pages
+                                        :following, :followers]
 	before_action :correct_user,   only: [:edit, :update]
 	before_action :admin_user,     only: :destroy
 
@@ -60,6 +62,20 @@ class UsersController < ApplicationController
     else
       render 'edit'
     end
+  end
+
+  def following
+    @title  = "Outgoing Friends"
+    @user   = User.find(params[:id])
+    @users  = @user.following.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title  = "Incoming Friends"
+    @user   = User.find(params[:id])
+    @users  = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
   end
 
 	private
