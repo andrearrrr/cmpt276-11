47d443fecfc603975bcc7010aafa8766088c17ec
Bryce-C <brycechutskoff@gmail.com>
2017-11-18 18:44:17 -0800

    Changed naming convention

--- app/models/friendship.rb
+++
@@ -1,6 +0,0 @@
-class Friendship < ApplicationRecord
-  belongs_to :send, class_name: "User"
-  belongs_to :recieve, class_name: "User"
-  validates :send_id, presence: true
-  validates :recieve_id, presence: true
-end
