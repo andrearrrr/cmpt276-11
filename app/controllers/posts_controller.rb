606a18b038e06fe2467b35dc8f0abcbbd4d2ca64
zullycomas <zullycomas32@gmail.com>
2017-11-20 00:09:12 -0800

    Create and delete posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
 before_action :logged_in_user, only: [:create, :destroy]
+before_action :correct_user,   only: :destroy
 
 	def create
 		@post = current_user.posts.build(post_params)
@@ -7,11 +8,15 @@ before_action :logged_in_user, only: [:create, :destroy]
 	      flash[:success] = "Post created!"
 	      redirect_to root_url
 	    else
-	      render 'posts/show'
+	      render 'posts/post_form'
 	    end
 	end
 
 	def destroy
+		@post.destroy
+    	flash[:success] = "Post deleted"
+    	redirect_to request.referrer || root_url
+
 	end
 
 	def index
@@ -25,5 +30,15 @@ before_action :logged_in_user, only: [:create, :destroy]
 		@post = current_user.posts.build if logged_in?
 
 	end
+
+	private
+
+    def post_params
+      params.require(:post).permit(:content)
+    end
+    def correct_user
+      @post = current_user.posts.find_by(id: params[:id])
+      redirect_to root_url if @post.nil?
+    end
 
 end
