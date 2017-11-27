67004d1f856988f419aaa0bdac974642e28dd1c1
shind07 <shind@sfu.ca>
2017-11-26 18:41:32 -0800

    update player pages to grab data from API

--- app/controllers/picks_controller.rb
+++ app/controllers/picks_controller.rb
@@ -2,7 +2,6 @@ class PicksController < ApplicationController
   def index
     @picks = Pick.all
     @awards = Award.all
-    @test = parse_test
   end
 
   def show
@@ -60,7 +59,7 @@ class PicksController < ApplicationController
     params.require(:pick).permit(:award_id, :player_id, :user_id, :league_id, :is_private, :season)
   end
 
-  def parse_test
+  def parse_stats(p_id)
     url = 'http://stats.nba.com/stats/playergamelog?PlayerID=200746&LeagueID=00&Season=2017-18&SeasonType=Regular%20Season'
     headers = {"Accept-Language": "en-us","User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Safari/604.1.38","Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", 'referer': 'http://stats.nba.com/'}
     resp = RestClient.get(url, headers = headers )
