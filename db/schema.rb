19a197be8611e77a160e06392f35127b1f92c3ad
shind07 <shind@sfu.ca>
2017-11-27 06:47:35 -0800

    Create Team model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171125185347) do
+ActiveRecord::Schema.define(version: 20171127144654) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -122,6 +122,16 @@ ActiveRecord::Schema.define(version: 20171125185347) do
     t.index ["followed_id"], name: "index_relationships_on_followed_id"
     t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
     t.index ["follower_id"], name: "index_relationships_on_follower_id"
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.integer "teamID"
+    t.string "abbreviation"
+    t.string "teamName"
+    t.string "simpleName"
+    t.string "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
