e5d999bd2a39c00620d11e8a89e68986eaf9f26d
Bryce-C <brycechutskoff@gmail.com>
2017-11-30 13:54:49 -0800

    Removed old micropost entries in schema.rb from previous merge error

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171120025431) do
+ActiveRecord::Schema.define(version: 20171124055501) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,21 +23,16 @@ ActiveRecord::Schema.define(version: 20171120025431) do
     t.string "description"
   end
 
+  create_table "contacts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "leagues", force: :cascade do |t|
     t.string "name"
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "microposts", force: :cascade do |t|
-    t.text "content"
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "picture"
-    t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
-    t.index ["user_id"], name: "index_microposts_on_user_id"
   end
 
   create_table "picks", force: :cascade do |t|
@@ -131,6 +126,5 @@ ActiveRecord::Schema.define(version: 20171120025431) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
-  add_foreign_key "microposts", "users"
   add_foreign_key "posts", "users"
 end
