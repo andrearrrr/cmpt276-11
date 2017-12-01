87e5f67fc7e1dc3d222634facf156099f4f56bd0
Bryce-C <brycechutskoff@gmail.com>
2017-11-30 16:08:53 -0800

    Added test for group name length + validation

--- test/models/group_test.rb
+++ test/models/group_test.rb
@@ -10,8 +10,13 @@ class GroupTest < ActiveSupport::TestCase
     assert @group.valid?
   end
 
-  test "should be present" do
+  test "name should be present" do
     @group.name = "    "
+    assert_not @group.valid?
+  end
+
+  test "name should be resonable length" do
+    @group.name = "a" * 101
     assert_not @group.valid?
   end
 end
