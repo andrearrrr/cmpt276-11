ee30afaf43dcf915c026d1308a54ac8303e9a8c1
Bryce-C <brycechutskoff@gmail.com>
2017-12-01 20:56:02 -0800

    Trying to fix bootstrap javascript issues

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
-//= require bootstrap
 //= require jquery
 //= require jquery-ui
 //= require jquery.turbolinks
+//= require rails-ujs
+//= require bootstrap
 //= require turbolinks
 //= require dataTables/jquery.dataTables
 //= require players
