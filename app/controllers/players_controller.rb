5cb20a3c1432bea568c57e6218b66091f6e456b2
Bryce-C <brycechutskoff@gmail.com>
2017-12-02 18:23:47 -0800

    Magic Fix?

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -12,7 +12,7 @@ class PlayersController < ApplicationController
   def parse_stats(p_id)
     url = "http://stats.nba.com/stats/playercareerstats?PlayerID=" + p_id + "&LeagueID=00&PerMode=PerGame"
     headers = {"Accept-Language": "en-us","User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Safari/604.1.38","Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", 'referer': 'http://stats.nba.com/'}
-    resp = RestClient::Request.execute(method: :get, url: url, headers: headers, timeout: -1)
+    resp = RestClient::Request.execute(method: :get, url: url, headers: headers, timeout: nil)
     return JSON.parse(resp)
   end
 
