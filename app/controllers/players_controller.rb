414282196b1faf2452214be3eaa37600ea14d698
shind07 <shind@sfu.ca>
2017-12-03 12:33:35 -0800

    Remove dynamic stats from Players Controller

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -2,18 +2,7 @@ require 'json'
 class PlayersController < ApplicationController
   def show
     @player = Player.find(params[:id])
-    @stats = parse_stats(@player.PERSON_ID.to_s)
-    @stats = @stats['resultSets'][0]['rowSet']
-
   end
 
-  private
-
-  def parse_stats(p_id)
-    url = "http://stats.nba.com/stats/playercareerstats?PlayerID=" + p_id + "&LeagueID=00&PerMode=PerGame"
-    headers = {"Accept-Language": "en-us","User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Safari/604.1.38","Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", 'referer': 'http://stats.nba.com/'}
-    resp = RestClient.get(url, headers = headers)
-    return JSON.parse(resp)
-  end
 
 end
